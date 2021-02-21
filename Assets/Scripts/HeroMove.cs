3e5ddc3d50619a66ea913cb17922081f6e09be31
Orange <leonid6845@mail.ru>
2021-02-22 02:18:31 +0400

    Demo

---
+++ Assets/Scripts/HeroMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+    public float speedHero = 20f;
+
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speedHero;
+
+        spriteRenderer.flipY = moveVelocity.y > 0.0f;
+        spriteRenderer.flipX = moveVelocity.x < 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + moveVelocity * Time.deltaTime);
+
+        
+
+    }
+}
