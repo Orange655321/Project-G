8de56f4907b286a6d72ee141c094a244d99b852a
FVT34U <captaintedotron55@gmail.com>
2021-02-23 16:11:14 +0400

    Сделал анимации, добавил звуки, теперь корректно

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject leaderboardMenu;
+    public GameObject settingsButtonExit;
+    public GameObject leaderboardButtonExit;
     public void PlayGame()
     {
+        StartCoroutine(StartTimer());       
+    }
+
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
     {
+        StartCoroutine(ExitTimer());
+    }
+
+    IEnumerator ExitTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
         Debug.Log("Exit!");
         Application.Quit();
     }
+
+
 }
