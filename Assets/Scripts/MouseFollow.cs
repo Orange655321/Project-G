c4595bb5a9701e73e77275b88801e0902c6d73a6
Orange <leonid6845@mail.ru>
2021-03-04 17:41:43 +0400

    Сделал пау пиу стрельбу из пистолета

--- Assets/Scripts/MouseFollow.cs
+++ Assets/Scripts/MouseFollow.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class MouseFollow : MonoBehaviour
 {
     
-
-
     // Update is called once per frame
     void Update()
     {
@@ -15,9 +13,6 @@ public class MouseFollow : MonoBehaviour
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //положение мыши из экранных в мировые координаты
         var angle = Vector2.Angle(Vector2.right, mousePosition - transform.position);//угол между вектором от объекта к мыше и осью х
         transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < mousePosition.y ? angle : -angle);//немного магии на последок
-
-
-
 
     }
 }
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform bulletPoint;
+    public GameObject prefabBullet;
+
+    public float bulletForce = 8f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))//жмакай ЛКМ
+        {
+            shoot();
+        }
+    }
+    void shoot()
+    {
+        GameObject bullet = Instantiate(prefabBullet, bulletPoint.position, bulletPoint.rotation);//создаем объект из префаба в месте поинта
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(bulletPoint.up * bulletForce, ForceMode2D.Impulse);//отправляем пулю из ствола на...
+    }
+}
