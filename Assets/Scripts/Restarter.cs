f731fc4c0a1763479bfed646084556666eb1b5ea
LexeyPivloy <hehpavlov1@gmail.com>
2021-03-15 14:15:18 +0400

    Сделал рандомную генерацию карты

---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restarter : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Application.Quit();
+        }
+    }
+}
