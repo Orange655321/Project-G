5e2064c528f32037a2ee5719db00d2f5b32e3bb6
FVT34U <captaintedotron55@gmail.com>
2021-03-25 11:07:28 +0400

    добавил пару анимаций, контроллер к ним, текстуру стены нормальную, кое-что переместил в папки для удобства

---
+++ Assets/Scripts/MainCharacter/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void RunAnimationOn()
+    {
+        animator.SetBool("isRunning", true);
+    }
+    public void RunAnimationOff()
+    {
+        animator.SetBool("isRunning", false);
+    }
+}
