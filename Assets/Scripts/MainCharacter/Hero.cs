d484b240dee2d377daf33ad6daf81778d86169eb
Orange <leonid6845@mail.ru>
2021-04-12 21:06:42 +0400

    Добавил хп, броню и счёт

---
+++ Assets/Scripts/MainCharacter/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hero : MonoBehaviour
+{
+    private int health;
+    private int armor;
+    private int score;
+    public Text healthText;
+    public Text armorText;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        armor = 0;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "" + health;
+        armorText.text = "" + armor;
+        scoreText.text = "" + score;
+    }
+
+    public void AddToScore(int cost) 
+    {
+        score += cost;
+    }
+}
