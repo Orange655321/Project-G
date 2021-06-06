7af654c5c117a6f80e1c2f8878ee73b81969147c
homnik2001 <79448448+homnik2001@users.noreply.github.com>
2021-06-06 21:23:47 +0400

    Update LobbyManager.cs

--- Assets/Scripts/MultiPlayer/LobbyManager.cs
+++ Assets/Scripts/MultiPlayer/LobbyManager.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class LobbyManager : MonoBehaviour
+using Photon.Pun;
+using UnityEngine.UI;
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    public Text LogText;
     void Start()
     {
-        
+        PhotonNetwork.NickName = "Player " + Random.Range(1, 100);
+        Log("Player's nickname is set to " + PhotonNetwork.NickName);
+        PhotonNetwork.GameVersion = "alpha v 1.0.0";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        Log("Connected to master");
+    }
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnConnected()
+    {
+        Log("Joined the room");
+        PhotonNetwork.LoadLevel("Coop");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Log(string message)
     {
-        
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
     }
 }
