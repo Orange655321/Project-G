f4942601e5e54df9cb2816516b7431c92ff0e6db
FVT34U <captaintedotron55@gmail.com>
2021-06-13 13:22:17 +0400

    не качать, это хуйня

---
+++ Assets/Scripts/UI/ProcStarter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class ProcStarter : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public GameObject proc;
+
+    IEnumerator startDelay()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        Time.timeScale = 1f;
+        proc.SetActive(false);
+        playableDirector.Play();
+    }
+
+    void Start()
+    {
+        proc.SetActive(true);
+        Time.timeScale = 0f;
+        StartCoroutine(startDelay());
+    }
+}
